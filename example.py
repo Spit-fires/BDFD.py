b74b837e0f54c5eded46c64ccf6736df14d2bcd5
akashfahad12@gmail.com <akashfahad12@gmail.com>
2022-07-31 15:17:43 +0600

    fix example bot

--- example.py
+++ example.py
@@ -1,7 +1,14 @@
 import bdfdpy as bdfd
 from decouple import config
 
+description = '''An example bot to show the usage of bdfdpy'''
+
 token = config('token')
 
+intents = bdfd.discord.Intents.default() 
+ bdfd.intents.members = True 
+ 
+bot = bdfd.commands.Bot(command_prefix = 'e!', description = description, intents = intents)
+ 
 bdfd.bot_run(token)
 bdfd.bot_command("e!ping", "pong")
